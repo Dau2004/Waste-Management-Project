2745349bb1e0032ee25bedaeb3255c765340a0ae
UMUTONI Kevine <138827957+simplykevine@users.noreply.github.com>
2024-06-21 09:56:18 +0200

    Update app.py

--- Last Work/Final_work/app.py
+++ Last Work/Final_work/app.py
@@ -3,7 +3,7 @@ from website import create_app
     This script runs a Flask web application.
 
     It imports the `create_app` function from the `website` module and creates an instance of the Flask app.
-    The app is then run in debug mode.
+    The app is then run in debugging mode.
 
     Usage:
         - Run this script to start the Flask web application.
